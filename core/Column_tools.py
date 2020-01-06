03f6ab3f8458a4046233a31a09e272532fb82090
allisong <gary.allison@gmail.com>
2020-01-06 11:29:32 -0500

    add Column_tools.py file

---
+++ core/Column_tools.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 16:46:40 2019
+
+@author: Gary
+These tools are used to add 'best guess' columns to tables from the xlate files.
+
+"""
+
+import pandas as pd
+
+fields = {'Supplier':'company',
+          'OperatorName':'company',
+          'StateName':'StateName'}
+
+def add_bg_col(df,field='StateName',sources='./sources/'):
+    print(f'Adding column {"bg"+field}')
+    fn = sources+fields[field]+'_'+'xlate.csv'
+    ref = pd.read_csv(fn,keep_default_na=False,na_values='',quotechar='$',
+                      usecols=['primary','original'])
+    df['original'] = df[field].str.strip().str.lower()
+    #print (f' -- translate file: {fn}, with columns: {ref.columns}')
+    ref = ref.rename(columns={'primary':'bg'+field})
+    ref = ref[~ref.duplicated(subset='original',keep='last')]
+    df = df.merge(ref,on='original',how='left',validate='m:1')
+    df = df.drop('original',axis=1)
+    return df
+
